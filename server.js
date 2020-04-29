46c039275e7e532d24459149bdac03a310aa66b6
Tyler Getsay <tylergetay@gmail.com>
2020-04-29 17:34:45 -0400

    init

---
+++ server.js
@@ -0,0 +1,13 @@
+const argv = require('yargs').argv
+const express = require('express');
+
+const app = express();
+const port = argv.port || 3000;
+
+app.get('/', (req, res) => res.send('Warship Sample App!'));
+
+app.get('/args', (req, res) => {
+    res.json(argv);
+})
+
+app.listen(port, () => console.log(`Example app listening at http://localhost:${port}`));
